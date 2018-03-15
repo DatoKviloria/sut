3e88ced4b4b0b3aaebd8c93e510668a4199c5373
dkvilo <dkviloria@gmail.com>
2018-03-15 12:06:17 -0700

    Connect web-ui to sut-server via websocket

--- examples/helpers.js
+++ examples/helpers.js
@@ -1,16 +1,27 @@
-const {sut} = require('../index');
-
+const { sut } = require('../index');
 /*
 * Define Halper Functions for your test
 * for access use: sut.helper.[name]
 */
-sut.include({
-  size: x => x.length,
-  upper: x => x.toUpperCase(),
-  exclime: x => x.concat('!'),
-  odd: x => (x % 2 == 0) ? true : false,
-  countDonw: num => {
-    if(num === 0){ return; }
-    this(num-1);
+sut.include(
+  {
+    
+    math: {
+      odd: x => (x % 2 == 0) ? true : false,
+      countDonw: num => {
+        if(num === 0){ return; }
+        this.countDonw(num-1);
+      }
+    },
+    
+    array: {
+      size: x => x.length
+    },
+    
+    string: {
+      upper: x => x.toUpperCase(),      
+      exclime: x => x.concat('!')
+    }
+
   }
-});
+);
